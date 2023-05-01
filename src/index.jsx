81d3bb18ac442c0a253108d52bb8187bc3047471
Parth Prajapati <pp41495233@gmail.com>
2023-05-01 20:51:02 +0530

    fix

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import "./style/index.css";
+import "./style/style.css";
+import "./style/header.css";
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <>
